27c5152aa4f48db54d99de84c6bcf2b379d5afc3
Moorthi kanthasamy <Moorthi.Kanthasamy@outlook.com>
2018-10-14 22:52:19 +0530

    antd design and md5 hash added

--- src/api/server.js
+++ src/api/server.js
@@ -1,8 +1,16 @@
 import express from "express";
+import bodyParser from "body-parser";
 import router from "../../src/ui/app/serverEntry";
+import md5 from "md5";
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static("./"));
 app.use(router);
 app.get("/", (req, res) => res.send(`I'm from server`));
-
+app.post("/md5hash", (req, res) => {
+  const { data } = req.body;
+  const hashValue = data ? md5(data) : "Invalid Input";
+  res.send({ hash: hashValue });
+});
 app.listen(3000, () => console.log("Server Running at 3000"));
